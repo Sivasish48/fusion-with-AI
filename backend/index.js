4238dcf5076198f5cbd2cdd3bda80f6247cc45bc
Sivasish Praharaj <sivasish48@gmail.com>
2024-07-04 22:36:15 +0530

    successfully applied bcryptjs for the signup

--- backend/index.js
+++ backend/index.js
@@ -3,17 +3,20 @@ const app = express();
 const cors = require("cors");
 const { default: mongoose } = require("mongoose");
 const User = require("./models/userModel");
-
+const bcrypt = require("bcryptjs");
 app.use(express.json());
 app.use(cors());
 
 mongoose.connect("mongodb+srv://sivasish48:HVL2qGFrtjiNTEum@fusion.wubagih.mongodb.net/")
 
+
+const salt = bcrypt.genSaltSync(10);
+
 app.post("/register", async (req, res) => {
     const{username, password} = req.body;
 
     try{
-    const userDoc = await User.create({username,password});
+    const userDoc = await User.create({username,password:bcrypt.hashSync(password,salt)});
       res.json(userDoc);
     }catch(err){
       console.log(err);
